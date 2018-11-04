b6ea446808e7e0f76b73f34c38f5e32017bac964
Acta86Neon <marco.meichtry.1986@gmail.com>
2018-11-04 13:56:12 +0100

    api routing

--- server/api.js
+++ server/api.js
@@ -2,7 +2,12 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+const minionRouter = require('./minions');
+const ideasRouter = require('./ideas');
+const meetingsRouter = require('./meetings');
 
-
+apiRouter.use('./minions', minionRouter);
+apiRouter.use('./ideas', ideasRouter);
+apiRouter.use('./meetings', meetingsRouter);
 
 module.exports = apiRouter;
